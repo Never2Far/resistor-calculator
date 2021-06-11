44ad52538e5f6d7dba6f5147249d3e694f159981
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-11 15:01:56 -0500

    Added Auth0 Componment wrapping App component

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
+  
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+        domain="near-net.us.auth0.com"
+        // domain="auth.near-net.com"
+        clientId="QoNmTgdo3zWNPxxFoeGc6p3Cu9aZhks4"
+        redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
