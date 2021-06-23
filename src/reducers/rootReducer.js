e162832296d5d0ed9e24b71dd6065bb7a5d989ee
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-23 18:25:43 -0500

    combine reducers

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import resistor from '../reducers/resistor'
 
 
-const rootReducer = combineReducers({
-    
-});
+
+const rootReducer = combineReducers(
+    {resistor: resistor}
+    );
+
 
 export default rootReducer
