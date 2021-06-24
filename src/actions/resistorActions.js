6be3b9702360f9002db388cfe8b3946f4b2e119e
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-23 21:31:54 -0500

    export bound action creators

---
+++ src/actions/resistorActions.js
@@ -0,0 +1,22 @@
+import { bindActionCreators } from "redux"
+
+const resisterActions = bindActionCreators({
+
+
+getValue
+
+
+
+    
+})
+
+
+
+    export function getValue(colorCode) {
+
+        return 'value placeholder'
+    
+    }
+
+
+    export default resisterActions
