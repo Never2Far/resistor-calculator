a52fe2eed71056ba312321c6cf81f458be6b2fb7
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-23 20:27:58 -0500

    added class names to color bands for styling

--- src/components/ColorBand.js
+++ src/components/ColorBand.js
@@ -3,12 +3,22 @@ import React from 'react'
 const ColorBand = (props) => {
 
 const bandColor = props.color
+const pos = props.pos
 
 
-return(
 
-    <span>{bandColor}</span>
-)
+if (pos === 'first'){
+    return(
+        <div className={`color-band-first ${bandColor}`}></div>
+    )
+}
+else if (pos === 'last'){
+   return( <div className={`color-band-last ${bandColor}`}></div>)
+}
+else
+    return( <div className={`color-band ${bandColor}`}></div>)
+
+
 
 }
 
