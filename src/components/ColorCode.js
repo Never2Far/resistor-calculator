bd853445ebaf6e21f113b525add886e5ad5ad413
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-23 20:28:41 -0500

    added function to return position of band for node's class name

--- src/components/ColorCode.js
+++ src/components/ColorCode.js
@@ -4,15 +4,35 @@ import ColorBand from './ColorBand'
 const ColorCode = () => {
   const   colorCode = ["yellow", "blue", "green", "gold"]
 
+  const checkPos = (color) => {
+      switch (colorCode.indexOf(color)) {
+          case 0:
+              return 'first'
+              
+              case colorCode.length - 1:
+                  return 'last'
+      
+          default:
+              return 'mid'
+      }
+  }
+
     return (
-<div>
+<>
         {colorCode.map(color => 
-            <ColorBand color={color} />
+
+
+
+
+            <ColorBand color={color} pos={checkPos(color)}/>
+
+            
+
             )}
         
-
+        </>
         
-        </div>
+        
     )
 
 }
