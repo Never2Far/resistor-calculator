636e32ffed42a87bae41ebc275efad81da016a11
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-25 16:25:09 -0500

    using dropdown menu buttons instead of columns of radio buttons

--- src/components/ColorPickerColumn.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ColorPickerColumn = () => {
-    return <span>Color Picker Column Placeholder</span>
-}
-
-export default ColorPickerColumn
---
+++ src/components/ColorPickerDropdown.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ColorPickerDropdown = () => {
+    return <span>Color Picker Dropdown Placeholder</span>
+}
+
+export default ColorPickerDropdown
