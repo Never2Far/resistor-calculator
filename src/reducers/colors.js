d1242dcc5bda1f562bc28d87b28f3b247810e2f2
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-24 20:41:16 -0500

    added fetching colors and associated action types to reducer

--- src/reducers/colors.js
+++ src/reducers/colors.js
@@ -1,10 +1,22 @@
-function colors(state = {}, action) {
+function colors(state = {colors: [],
+loading: false,
+loaded: false}, action) {
 
 
-switch (action) {
-    case 'FETCH_COLORS':
-        
-        return state
+switch (action.type) {
+    case 'FETCHING_COLORS':
+        console.log('Fetching Colors...')
+        return Object.assign({}, {...state}, {colors: [...state.colors]}, {loading: true})
+
+        case 'ADD_COLOR':
+            
+            
+            
+            return Object.assign({}, {...state}, {colors: [...state.colors, action.payload]} )
+
+            case 'ALL_COLORS_ADDED':
+                console.log("All colors added!")
+                return Object.assign({}, {...state}, {loading: false,loaded: true})
 
     default:
         return state
