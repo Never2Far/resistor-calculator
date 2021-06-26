eefab79132f16a35c2b87f50b72703d845d330d3
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-26 08:47:23 -0500

    number of displayed color bands is now tied to band count

--- src/actions/resistorActions.js
+++ src/actions/resistorActions.js
@@ -6,8 +6,35 @@ export function getColorCodeFromValue(value) {
     return ['yellow', 'blue', 'green', 'gold']
 }
 
+export function setColorCode(colorCode) {
+    return (dispatch) => {
+        dispatch({ type: 'SET_COLOR_CODE', payload: colorCode })
+    }
+}
+
 export function setBandCount(count) {
+    let colorCode
+    switch (count) {
+        case 3:
+            colorCode = ['yellow', 'blue', 'gold']
+            break
+        case 4:
+            colorCode = ['yellow', 'blue', 'green', 'gold']
+            break
+        case 5:
+            colorCode = ['yellow', 'blue', 'green', 'red', 'gold']
+            break
+        case 6:
+            colorCode = ['yellow', 'blue', 'black', 'green', 'red', 'gold']
+            break
+
+        default:
+            colorCode = ['yellow', 'blue', 'green', 'gold']
+            break
+    }
+
     return (dispatch) => {
         dispatch({ type: 'SET_BAND_COUNT', payload: count })
+        dispatch({ type: 'SET_COLOR_CODE', payload: colorCode })
     }
 }
