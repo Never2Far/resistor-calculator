92c50e31de8c4e34ee1369fa6f670d24bfd42a12
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-25 20:16:42 -0500

    added action to set band count

--- src/actions/resistorActions.js
+++ src/actions/resistorActions.js
@@ -1,11 +1,15 @@
-import { bindActionCreators } from 'redux'
-
 export function getValue(colorCode) {
     return 'value placeholder'
 }
 
 export function getColorCodeFromValue(value) {
     return ['yellow', 'blue', 'green', 'gold']
+}
+
+export function setBandCount(count) {
+    return (dispatch) => {
+        dispatch({ type: 'SET_BAND_COUNT', payload: count })
+    }
 }
 
 export default resistorActions
