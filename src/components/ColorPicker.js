1f123f430d9bf835b9befaaa21d988928272ca1d
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-25 20:22:47 -0500

    container for color choices representing the colors of bands

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const ColorPicker = () => {
-    return <span>Color Picker Placeholder</span>
+    const bandCount = useSelector((state) => state.resistor.bandCount)
+
+    return <span>Color Picker Placeholder: {bandCount}</span>
 }
 
 export default ColorPicker
