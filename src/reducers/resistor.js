162486820492f120e90c426ed3c390d1ad1295c0
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-26 15:47:57 -0500

    added resistor value attributes to state

--- src/reducers/resistor.js
+++ src/reducers/resistor.js
@@ -1,19 +1,48 @@
+// this.digit1 = digit1;
+// this.digit2 = digit2;
+// this.digit3 = digit3;
+// this.multiplier = multiplier;
+// this.tolerance = tolerance;
+// this.tempCoef = tempCoef;
+
 function resistor(
     state = {
-        colorCode: ['yellow', 'blue', 'green', 'gold'],
+        colorCode: ['yellow', 'violet', 'black', 'gold'],
         bandCount: 4,
-        value: 500,
+        value: 47,
+        digit1: 4,
+        digit2: 7,
+        digit3: null,
+        multiplier: 1,
+        tolerance: 0.05,
+        tempCoefficient: null,
     },
     action
 ) {
     switch (action.type) {
         case 'SET_BAND_COUNT':
-            console.log(`band count: ${action.payload}`)
-            return Object.assign(
-                {},
-                { ...state },
-                { bandCount: action.payload }
-            )
+            // const newState = Object.assign({}, { ...state })
+            // newState.bandCount = action.payload
+            const newState = {}
+            newState.bandCount = action.payload
+            switch (action.payload) {
+                case 5:
+                    newState.tempCoefficient = null
+                    break
+                case 4:
+                    newState.tempCoefficent = null
+                    newState.tolerance = null
+                    break
+                case 3:
+                    newState.tempCoefficient = null
+                    newState.tolerance = null
+                    newState.digit3 = null
+                    break
+                default:
+                    break
+            }
+            console.log(`band count: ${action.payload}, new state: ${newState}`)
+            return Object.assign({}, { ...state }, newState)
 
         case 'SET_COLOR_CODE':
             console.log(`color code: ${action.payload}`)
@@ -22,6 +51,10 @@ function resistor(
                 { ...state },
                 { colorCode: action.payload }
             )
+
+        case 'SET_VALUE':
+            console.log(action.payload)
+            return Object.assign({}, { ...state }, action.payload)
         default:
             return state
     }
