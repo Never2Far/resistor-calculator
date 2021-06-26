f725dc01dcf5c92d9f85f45e1d515e78b767e99c
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-25 20:23:10 -0500

    dropdown with color choices

--- src/components/ColorPickerDropdown.js
+++ src/components/ColorPickerDropdown.js
@@ -1,6 +1,10 @@
 import React from 'react'
+import SplitButton from 'react-bootstrap/SplitButton'
 
-const ColorPickerDropdown = () => {
+const ColorPickerDropdown = (props) => {
+    const title = props.title
+    const choices = props.choices
+
     return <span>Color Picker Dropdown Placeholder</span>
 }
 
