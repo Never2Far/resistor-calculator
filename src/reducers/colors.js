bac39918a9d425857fbc666fbad4bf731ed11572
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-28 14:10:51 -0500

    changed array of color objects to array of variables pointing to color objects

--- src/reducers/colors.js
+++ src/reducers/colors.js
@@ -17,10 +17,39 @@ function colors(state = { colors: [], loading: false, loaded: false }, action) {
             )
 
         case 'ALL_COLORS_ADDED':
+            const [
+                BLACK,
+                BROWN,
+                RED,
+                ORANGE,
+                YELLOW,
+                GREEN,
+                BLUE,
+                VIOLET,
+                GREY,
+                WHITE,
+                GOLD,
+                SILVER,
+            ] = state.colors
+            const colors = {
+                BLACK,
+                BROWN,
+                RED,
+                ORANGE,
+                YELLOW,
+                GREEN,
+                BLUE,
+                VIOLET,
+                GREY,
+                WHITE,
+                GOLD,
+                SILVER,
+            }
             console.log('All colors added!')
             return Object.assign(
                 {},
                 { ...state },
+                { colors },
                 { loading: false, loaded: true }
             )
 
