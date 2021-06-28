3d5cb7843dc9ebe76d53a86030613f293bb25642
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-28 14:12:43 -0500

    toggle button for color choice within column

--- src/components/ColorChoice.js
+++ src/components/ColorChoice.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import ToggleButton from 'react-bootstrap/esm/ToggleButton'
 
-const ColorChoice = () => {
-    return <span>Color Choice Placeholder</span>
+const ColorChoice = (props) => {
+    const color = props.color
+
+    return (
+        <ToggleButton type="radio" value={color.name} className={color.name}>
+            {color.name}
+        </ToggleButton>
+    )
 }
 
 export default ColorChoice
