03fe04983d9c891e7fd14f3f32c3b1148de8822c
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-06-28 19:36:09 -0500

    toggle buttons (radios) for each color option in a column

--- src/components/ColorChoice.js
+++ src/components/ColorChoice.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ToggleButton from 'react-bootstrap/esm/ToggleButton'
+import { setDigit } from '../actions/resistorActions'
 
 const ColorChoice = (props) => {
     const color = props.color
 
     return (
-        <ToggleButton type="radio" value={color.name} className={color.name}>
+        <ToggleButton
+            type="radio"
+            value={color.name}
+            className={color.name}
+            onClick={(e) => props.handleClick(color.name)}
+        >
             {color.name}
         </ToggleButton>
     )
