e37ef03aa041b4e35fe9ae1981bc54103e91e0c6
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-07-06 18:13:25 -0500

    displays value represented by color code

--- src/components/ValueDisplay.js
+++ src/components/ValueDisplay.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
+import { useSelector } from 'react-redux'
 
-const ValueDisplay = () => {
-    return <span>Value Display Placeholder</span>
+const ValueDisplay = (props) => {
+    // const resistor = useSelector((state) => state.resistor)
+
+    // const [, updateState] = useState()
+    // const forceUpdate = useCallback(() => updateState({}), [])
+    const resistor = props.resistor
+
+    function displayValue() {
+        if (resistor.tempCoefficient != null) {
+            return `${resistor.value} \u03A9 (Ohms)  +/- ${resistor.tolerance}%  ${resistor.tempCoefficient}ppm/K`
+        } else if (resistor.tolerance == null) {
+            return `${resistor.value} \u03A9 (Ohms) `
+        } else {
+            return `${resistor.value} \u03A9 (Ohms)  +/- ${resistor.tolerance}%`
+        }
+    }
+
+    return <span id="component-value">{displayValue()}</span>
 }
 
 export default ValueDisplay
