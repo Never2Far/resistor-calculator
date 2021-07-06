fc6e078e91983e7663dbe6b69dd92f801a65e2d4
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-07-06 18:11:52 -0500

    fixed tempCoefficient typo

--- src/components/ColorPickerColumn.js
+++ src/components/ColorPickerColumn.js
@@ -3,19 +3,36 @@ import { useSelector, useDispatch } from 'react-redux'
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
 import ToggleButton from 'react-bootstrap/ToggleButton'
 import { colorFromDigit, choicesFromColumnName } from '../utilities/utility'
+import {
+    updateColorCode,
+    setValueFromColorCode,
+    setDigit,
+    updateValue,
+} from '../actions/resistorActions'
 
 const ColorPickerColumn = (props) => {
     const columnName = props.columnName[0]
     const columnDisplayName = props.columnName[1]
+    const currentColor = props.columnName[2]
+    const codeIndex = props.columnName[3]
+    const colorCode = props.colorCode
+    console.log(currentColor)
+    const dispatch = useDispatch()
     console.log(props.COLORS)
-    console.log(props.choices)
+
     // const colorChoices = choicesFromColumnName(columnName, props.COLORS)
 
-    const [columnValue, setColumnValue] = useState('yellow')
+    const [columnValue, setColumnValue] = useState(currentColor)
 
-    const handleChange = (value) => {
-        console.log(value)
-        setColumnValue(value)
+    const handleChange = (colorName) => {
+        console.log(colorName)
+        setColumnValue(colorName)
+        dispatch(updateColorCode(colorName, codeIndex))
+        // dispatch(setValueFromColorCode(colorCode))
+        dispatch(setDigit(columnName, colorName))
+        dispatch(updateValue())
+
+        // dispatch()
     }
 
     // const COLORS = Object.assign(
@@ -45,7 +62,7 @@ const ColorPickerColumn = (props) => {
             colorKeys.pop()
             colorKeys.pop()
             break
-        case 'temp-coef':
+        case 'tempCoefficient':
             colorKeys.pop()
             colorKeys.pop()
             colorKeys.pop()
@@ -76,10 +93,11 @@ const ColorPickerColumn = (props) => {
                     onChange={handleChange}
                     value={columnValue}
                 >
-                    {colorKeys.map((color) => (
+                    {colorKeys.map((color, idx) => (
                         <ToggleButton
                             className={props.COLORS[color].name}
                             value={props.COLORS[color].name}
+                            key={idx}
                         >
                             {props.COLORS[color].name}
                         </ToggleButton>
