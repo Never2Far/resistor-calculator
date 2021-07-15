565208700168c93d7303bec08905daad478f7068
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-07-15 14:08:36 -0500

    actions now update colorbands and value

--- src/actions/resistorActions.js
+++ src/actions/resistorActions.js
@@ -11,6 +11,7 @@ export function setDigit(digitName, color) {
         else value = colors[color.toUpperCase()][digitName]
 
         dispatch({ type: 'SET_DIGIT', payload: { digitName, value } })
+        dispatch(updateValue())
     }
 }
 
@@ -28,11 +29,11 @@ export function updateValue() {
                 ) * resistor.multiplier
         } else if (resistor.tolerance === null) {
             value =
-                parseInt(`${resistor.digit1}` + `${resistor.digit2}`) *
+                parseInt(`${resistor.digit1}${resistor.digit2}`) *
                 resistor.multiplier
         } else if (resistor.digit3 === null) {
             value =
-                parseInt(`${resistor.digit1}` + `${resistor.digit2}`) *
+                parseInt(`${resistor.digit1}${resistor.digit2}`) *
                 resistor.multiplier
         } else {
             value =
@@ -74,60 +75,97 @@ export function updateValue() {
 //     }
 // }
 
-// export function setValueFromColorCode(colorCode) {
-//     return (dispatch, getState) => {
-//         const bandCount = colorCode.length
-//         const colors = getState().colors.colors
-//         console.log(colors)
-//         // for (const color in colorCode) {
-//         const digits = {}
-//         // let value
-//         // }
-//         // let x
-//         // for (color in colorCode) {
-//         switch (bandCount) {
-//             case 3:
-//             case 4:
-//                 digits.digit1 = colors[colorCode[0].toUpperCase()].digit1
-//                 digits.digit2 = colors[colorCode[1].toUpperCase()].digit2
-//                 digits.multiplier =
-//                     colors[colorCode[2].toUpperCase()].multiplier
-//                 digits.value =
-//                     parseInt(`${digits.digit1}` + `${digits.digit2}`) *
-//                     digits.multiplier
-//                 console.log(digits.value)
-//                 break
-//             case 5:
-//             case 6:
-//                 digits.digit1 = colors[colorCode[0].toUpperCase()].digit1
-//                 digits.digit2 = colors[colorCode[1].toUpperCase()].digit2
-//                 digits.digit3 = colors[colorCode[2].toUpperCase()].digit3
-//                 digits.multiplier =
-//                     colors[colorCode[3].toUpperCase()].multiplier
-//                 digits.value =
-//                     parseInt(
-//                         `${digits.digit1}` +
-//                             `${digits.digit2}` +
-//                             `${digits.digit3}`
-//                     ) * digits.multiplier
-//                 console.log(digits)
-//                 break
-//             default:
-//                 break
-//         }
-//         // setColorCode(colorCode)
-//         // dispatch({type: 'SET_COLOR_CODE', payload: colorCode})
-//         dispatch({ type: 'SET_VALUE', payload: digits })
-//     }
-//     // }
-// }
+export function setValueFromColorCode(colorCode) {
+    return (dispatch, getState) => {
+        const bandCount = colorCode.length
+        const colors = getState().colors.colors
+        console.log(colors)
+        // for (const color in colorCode) {
+        const digits = {}
+        // let value
+        // }
+        // let x
+        // for (color in colorCode) {
+        dispatch({
+            type: 'SET_DIGIT',
+            payload: { digitName: 'digit3', value: null },
+        })
+        dispatch({
+            type: 'SET_DIGIT',
+            payload: { digitName: 'tolerance', value: null },
+        })
+        dispatch({
+            type: 'SET_DIGIT',
+            payload: { digitName: 'tempCoefficient', value: null },
+        })
+
+        switch (bandCount) {
+            case 3:
+                dispatch(setDigit('digit1', colorCode[0]))
+                dispatch(setDigit('digit2', colorCode[1]))
+                dispatch(setDigit('multiplier', colorCode[2]))
+                break
+            case 4:
+                dispatch(setDigit('digit1', colorCode[0]))
+                dispatch(setDigit('digit2', colorCode[1]))
+                dispatch(setDigit('multiplier', colorCode[2]))
+                dispatch(setDigit('tolerance', colorCode[3]))
+                // digits.digit1 = colors[colorCode[0].toUpperCase()].digit1
+                // digits.digit2 = colors[colorCode[1].toUpperCase()].digit2
+                // digits.multiplier =
+                //     colors[colorCode[2].toUpperCase()].multiplier
+                // digits.value =
+                //     parseInt(`${digits.digit1}` + `${digits.digit2}`) *
+                //     digits.multiplier
+                // console.log(digits.value)
+
+                break
+            case 5:
+                dispatch(setDigit('digit1', colorCode[0]))
+                dispatch(setDigit('digit2', colorCode[1]))
+                dispatch(setDigit('digit3', colorCode[2]))
+                dispatch(setDigit('multiplier', colorCode[3]))
+                dispatch(setDigit('tolerance', colorCode[4]))
+                break
+            case 6:
+                dispatch(setDigit('digit1', colorCode[0]))
+                dispatch(setDigit('digit2', colorCode[1]))
+                dispatch(setDigit('digit3', colorCode[2]))
+                dispatch(setDigit('multiplier', colorCode[3]))
+                dispatch(setDigit('tolerance', colorCode[4]))
+                dispatch(setDigit('tempCoefficient', colorCode[5]))
+                break
+            // digits.digit1 = colors[colorCode[0].toUpperCase()].digit1
+            // digits.digit2 = colors[colorCode[1].toUpperCase()].digit2
+            // digits.digit3 = colors[colorCode[2].toUpperCase()].digit3
+            // digits.multiplier =
+            //     colors[colorCode[3].toUpperCase()].multiplier
+            // digits.value =
+            //     parseInt(
+            //         `${digits.digit1}` +
+            //             `${digits.digit2}` +
+            //             `${digits.digit3}`
+            //     ) * digits.multiplier
+            // console.log(digits)
+
+            default:
+                break
+        }
+        // setColorCode(colorCode)
+        // dispatch({type: 'SET_COLOR_CODE', payload: colorCode})
+        // dispatch({ type: 'SET_VALUE', payload: digits })
+        dispatch(updateValue())
+    }
+    // }
+}
 
 export function updateColorCode(newColorName, codeIndex) {
     return (dispatch, getState) => {
         const newColorCode = [...getState().resistor.colorCode]
         newColorCode[codeIndex] = newColorName
-        // setValueFromColorCode(newColorCode)
+
         dispatch({ type: 'SET_COLOR_CODE', payload: newColorCode })
+        setValueFromColorCode(newColorCode)
     }
 }
 
@@ -152,12 +190,12 @@ export function setValue(value) {
         switch (bandCount) {
             case 3:
             case 4:
-                newValue.multiplier = parseInt(`1` + `${digits.join('')}`)
+                newValue.multiplier = parseInt(`1${digits.join('')}`)
                 break
             case 5:
             case 6:
                 newValue.digit3 = parseInt(digits.shift())
-                newValue.multiplier = parseInt(`1` + `${digits.join('')}`)
+                newValue.multiplier = parseInt(`1${digits.join('')}`)
                 break
             default:
                 break
@@ -191,6 +229,8 @@ export function setBandCount(count) {
     return (dispatch) => {
         dispatch({ type: 'SET_BAND_COUNT', payload: count })
         dispatch({ type: 'SET_COLOR_CODE', payload: colorCode })
-        dispatch(updateValue())
+
+        dispatch(setValueFromColorCode(colorCode))
+        // dispatch(updateValue())
     }
 }
