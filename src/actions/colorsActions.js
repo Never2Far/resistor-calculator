e3b5c4587d883fd360d58a5268808be2d633677a
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-07-15 14:07:45 -0500

    fixed typo and changed action types to past tense

--- src/actions/colorsActions.js
+++ src/actions/colorsActions.js
@@ -1,11 +1,11 @@
 export function fetchColors() {
     return (dispatch) => {
-        console.log('dispacthging')
+        console.log('dispatching')
         dispatch({ type: 'FETCHING_COLORS' })
         fetch(process.env.REACT_APP_COLORS_URL)
             .then((response) => response.json())
             .then((colors) => {
-                console.log(colors)
+                // console.log(colors)
                 for (const color of colors.data) {
                     const newColor = {
                         name: color.attributes.name,
@@ -14,10 +14,10 @@ export function fetchColors() {
                         tolerance: color.attributes.tolerance,
                         tempCoefficient: color.attributes.temp_coefficient,
                     }
-                    console.log(newColor)
-                    dispatch({ type: 'ADD_COLOR', payload: newColor })
+                    // console.log(newColor)
+                    dispatch({ type: 'COLOR_FETCHED', payload: newColor })
                 }
-                dispatch({ type: 'ALL_COLORS_ADDED' })
+                dispatch({ type: 'ALL_COLORS_FETCHED' })
             })
     }
 }
