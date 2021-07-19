b2481820c0d8bb3571b37e7978415e5ba221f859
Never2Far <30479783+Never2Far@users.noreply.github.com>
2021-07-18 20:48:44 -0500

    added Auth0 components back in

--- src/App.js
+++ src/App.js
@@ -8,14 +8,18 @@ import BandCountPicker from './components/BandCountPicker'
 import ColorPicker from './components/ColorPicker'
 // import ValueDisplay from './components/ValueDisplay'
 import { useSelector } from 'react-redux'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function App() {
     const colorsLoaded = useSelector((state) => state.colors.loaded)
     const resistor = useSelector((state) => state.resistor)
 
+    const { user, isAuthenticated } = useAuth0()
+
     if (colorsLoaded)
         return (
             <Container id="page-container">
+                {isAuthenticated ? <LogoutButton /> : <LoginButton />}
                 <Resistor resistor={resistor} />
                 <div id={'band-count-picker-text'} className={'centered'}>
                     {'Choose the number of bands on the resistor: '}
